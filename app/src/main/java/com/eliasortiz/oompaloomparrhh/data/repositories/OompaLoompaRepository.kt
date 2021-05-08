2de1e379f9e1934f90b975ad59af7bf839c2c49a
Elias Ortiz <elias.ortizreyes@gmail.com>
2021-05-08 21:41:48 +0200

    Create basic repository and Coroutines utils class

---
+++ app/src/main/java/com/eliasortiz/oompaloomparrhh/data/repositories/OompaLoompaRepository.kt
@@ -0,0 +1,24 @@
+package com.eliasortiz.oompaloomparrhh.data.repositories
+
+import com.eliasortiz.oompaloomparrhh.data.network.OompaLoompaApi
+import com.eliasortiz.oompaloomparrhh.data.network.OompaLoompaService
+import com.eliasortiz.oompaloomparrhh.data.network.SafeApiRequest
+
+class OompaLoompaRepository(private val api: OompaLoompaService) : SafeApiRequest() {
+
+    suspend fun getOompaLoompasList(page: Int) = apiRequest { api.getOompaLoompas(page) }
+
+    suspend fun getOompaLoompa(id: Int) = apiRequest { api.getOompaLoompa(id) }
+
+    companion object {
+        private var instance: OompaLoompaRepository? = null
+        fun getInstance(): OompaLoompaRepository {
+            instance?.let {
+                return it
+            } ?: run {
+                instance = OompaLoompaRepository(OompaLoompaApi.getInstance())
+                return instance!!
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/eliasortiz/oompaloomparrhh/utils/Coroutines.kt
@@ -0,0 +1,10 @@
+package com.eliasortiz.oompaloomparrhh.utils
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+object Coroutines {
+
+    fun main(work: suspend (() -> Unit)) = CoroutineScope(Dispatchers.Main).launch { work() }
+}
