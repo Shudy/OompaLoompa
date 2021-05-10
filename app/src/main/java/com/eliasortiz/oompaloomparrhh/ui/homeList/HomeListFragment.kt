bb0f3af886f7669a06b6fbfefbeea5c4c79b4376
Elias Ortiz <elias.ortizreyes@gmail.com>
2021-05-10 20:12:43 +0200

    Logic to handle the filters actions

--- app/src/main/java/com/eliasortiz/oompaloomparrhh/ui/homeList/HomeListFragment.kt
+++ app/src/main/java/com/eliasortiz/oompaloomparrhh/ui/homeList/HomeListFragment.kt
@@ -4,20 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.eliasortiz.oompaloomparrhh.R
 import com.eliasortiz.oompaloomparrhh.data.models.OompaLoompa
 import com.eliasortiz.oompaloomparrhh.data.network.NetworkConnectionInterception
 import com.eliasortiz.oompaloomparrhh.data.network.OompaLoompaApi
 import com.eliasortiz.oompaloomparrhh.data.repositories.OompaLoompaRepository
 import com.eliasortiz.oompaloomparrhh.databinding.HomeListFragmentBinding
 import com.eliasortiz.oompaloomparrhh.ui.adapters.OompaLoompaAdapter
+import com.eliasortiz.oompaloomparrhh.ui.customViews.FiltersBottomSheet
 import com.eliasortiz.oompaloomparrhh.ui.listeners.OompaLoompaListListener
 import com.eliasortiz.oompaloomparrhh.ui.recyclerDecorators.OompaLoompaDecorator
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.snackbar.Snackbar
 
 class HomeListFragment : Fragment(), OompaLoompaListListener {
@@ -63,6 +65,8 @@ class HomeListFragment : Fragment(), OompaLoompaListListener {
         setRecylerView()
 
         setViewModelObservers()
+        setClickListeners()
+        setListeners()
     }
 
     private fun setRecylerView() {
@@ -73,6 +77,11 @@ class HomeListFragment : Fragment(), OompaLoompaListListener {
             addItemDecoration(OompaLoompaDecorator())
             addOnScrollListener(object : RecyclerView.OnScrollListener() {
                 override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    // Fab control
+                    if (dy > 0 || (dy < 0 && binding.fab.isShown)) {
+                        binding.fab.hide()
+                    }
+
                     if (dy > 0) {
                         (recyclerView.layoutManager as? LinearLayoutManager)?.let { layoutManager ->
                             visibleItemCount = layoutManager.childCount
@@ -87,6 +96,14 @@ class HomeListFragment : Fragment(), OompaLoompaListListener {
                         }
                     }
                 }
+
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    // Fab control
+                    if (newState == RecyclerView.SCROLL_STATE_IDLE && !binding.filter.isActive()) {
+                        binding.fab.show()
+                    }
+                    super.onScrollStateChanged(recyclerView, newState)
+                }
             })
         }
     }
@@ -94,6 +111,7 @@ class HomeListFragment : Fragment(), OompaLoompaListListener {
     private fun setViewModelObservers() {
         viewModel.getOompaLoompaListLiveData()
             .observe(viewLifecycleOwner) { oompaLoompaListResponse ->
+                oompaLoompaList.clear()
                 oompaLoompaList.addAll(oompaLoompaListResponse)
                 oompaLoompaAdapter.notifyDataSetChanged()
             }
@@ -110,6 +128,11 @@ class HomeListFragment : Fragment(), OompaLoompaListListener {
                 viewModel.cleanError()
             }
         }
+
+        viewModel.getOptionsListLiveData().observe(viewLifecycleOwner) {
+            binding.filter.setGenderOptions(getString(R.string.genderTitle), it.first)
+            binding.filter.setProfessionOptions(getString(R.string.professionTitle), it.second)
+        }
     }
 
     override fun onDestroy() {
@@ -117,8 +140,34 @@ class HomeListFragment : Fragment(), OompaLoompaListListener {
         _binding = null
     }
 
+    private fun setClickListeners() {
+        binding.fab.setOnClickListener {
+            binding.fab.hide()
+            binding.filter.show()
+        }
+    }
+
+    fun setListeners() {
+        binding.filter.setListener(object : FiltersBottomSheet.FiltersListener {
+            override fun genderFilterOptions(genderOption: String) {
+                viewModel.applyGenderFilter(genderOption)
+            }
+
+            override fun professionFilterOptions(professionOption: String) {
+                viewModel.applyProfessionFilter(professionOption)
+            }
+
+            override fun filtersBottomSheetStateChanged(state: Int) {
+                if (state == BottomSheetBehavior.STATE_EXPANDED) {
+                    binding.fab.hide()
+                } else {
+                    binding.fab.show()
+                }
+            }
+        })
+    }
+
     override fun onOompaLoompaClicked(id: Int) {
-        Toast.makeText(context, "OompaLoompa Clicked: $id", Toast.LENGTH_SHORT).show()
         findNavController().navigate(HomeListFragmentDirections.actionHomeListFragmentToDetailFragment())
     }
 
--- app/src/main/java/com/eliasortiz/oompaloomparrhh/ui/homeList/HomeListViewModel.kt
+++ app/src/main/java/com/eliasortiz/oompaloomparrhh/ui/homeList/HomeListViewModel.kt
@@ -3,6 +3,7 @@ package com.eliasortiz.oompaloomparrhh.ui.homeList
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.eliasortiz.oompaloomparrhh.data.models.FilterOptionWithStatus
 import com.eliasortiz.oompaloomparrhh.data.models.OompaLoompa
 import com.eliasortiz.oompaloomparrhh.data.network.reponse.OompaLoompasResponse
 import com.eliasortiz.oompaloomparrhh.data.repositories.OompaLoompaRepository
@@ -10,17 +11,34 @@ import com.eliasortiz.oompaloomparrhh.utils.Coroutines
 import com.eliasortiz.oompaloomparrhh.utils.ResultResponse
 import timber.log.Timber
 
+private const val THRESHOLD = 10
+
 class HomeListViewModel(private val repository: OompaLoompaRepository) : ViewModel() {
 
     private val oompaLoompaList: MutableList<OompaLoompa> = mutableListOf()
+    private var oompaLoompaFilteredList: MutableList<OompaLoompa> = mutableListOf()
+
     private val oompaLoompaListLiveData: MutableLiveData<List<OompaLoompa>> =
         MutableLiveData(oompaLoompaList)
+
     private val isLoadingData: MutableLiveData<Boolean> = MutableLiveData(false)
     private val showErrorLiveData: MutableLiveData<Pair<Boolean, String>> =
         MutableLiveData(Pair(false, ""))
 
     private var lastPageRequested = 0
     private var hasMorePages = true
+
+    private var genderList: MutableList<FilterOptionWithStatus> = mutableListOf()
+    private var professionList: MutableList<FilterOptionWithStatus> = mutableListOf()
+
+    private val filterOptionsLiveData: MutableLiveData<Pair<List<FilterOptionWithStatus>, List<FilterOptionWithStatus>>> =
+        MutableLiveData(
+            Pair(genderList, professionList)
+        )
+
+    private var activeGenderFilter = ""
+    private var activeProfessionFilter = ""
+    private var filterIsActive = false
 
     init {
         getNewOompaLoompasPage()
@@ -31,6 +49,9 @@ class HomeListViewModel(private val repository: OompaLoompaRepository) : ViewMod
     fun getIsLoadingDataLiveData(): LiveData<Boolean> = isLoadingData
 
     fun getShowErrorLiveData(): LiveData<Pair<Boolean, String>> = showErrorLiveData
+
+    fun getOptionsListLiveData(): LiveData<Pair<List<FilterOptionWithStatus>, List<FilterOptionWithStatus>>> =
+        filterOptionsLiveData
 
     fun getNewOompaLoompasPage() {
         if (hasMorePages) {
@@ -53,8 +74,10 @@ class HomeListViewModel(private val repository: OompaLoompaRepository) : ViewMod
                         isLoadingData.postValue(false)
                         showErrorLiveData.postValue(Pair(true, response.message))
                     }
+
                     ResultResponse.Loading -> {
                     }
+
                     is ResultResponse.Success -> {
                         isLoadingData.postValue(false)
 
@@ -62,7 +85,7 @@ class HomeListViewModel(private val repository: OompaLoompaRepository) : ViewMod
 
                         data.current?.let { currentPage ->
                             data.total?.let { maxPages ->
-                                if (currentPage == 3) {
+                                if (currentPage == maxPages) {
                                     hasMorePages = false
                                 }
                             }
@@ -70,7 +93,14 @@ class HomeListViewModel(private val repository: OompaLoompaRepository) : ViewMod
 
                         data.results?.filterNotNull()?.let {
                             oompaLoompaList.addAll(it)
-                            oompaLoompaListLiveData.postValue(oompaLoompaList)
+
+                            if (filterIsActive) {
+                                applyFilters()
+                            } else {
+                                oompaLoompaListLiveData.postValue(oompaLoompaList)
+                            }
+
+                            refreshFiltersList(it)
                         }
                     }
                 }
@@ -78,7 +108,80 @@ class HomeListViewModel(private val repository: OompaLoompaRepository) : ViewMod
         }
     }
 
+    private fun refreshFiltersList(oompaLoompaList: List<OompaLoompa>) {
+        oompaLoompaList.forEach { oompaLoompa ->
+            oompaLoompa.gender?.let { gender ->
+                val position = genderList.indexOfFirst { it.optionTag == gender }
+                if (position < 0) {
+                    genderList.add(FilterOptionWithStatus(gender))
+                } else {
+                    genderList[position].isChecked =
+                        genderList[position].optionTag == activeGenderFilter
+                }
+            }
+
+            oompaLoompa.profession?.let { profession ->
+                val position = professionList.indexOfFirst { it.optionTag == profession }
+                if (position < 0) {
+                    professionList.add(FilterOptionWithStatus(profession))
+                } else {
+                    professionList[position].isChecked =
+                        professionList[position].optionTag == activeProfessionFilter
+                }
+
+            }
+        }
+
+        filterOptionsLiveData.postValue(Pair(genderList, professionList))
+    }
+
     fun cleanError() {
         showErrorLiveData.postValue(Pair(false, ""))
+    }
+
+    fun applyGenderFilter(genderOption: String) {
+        activeGenderFilter = genderOption
+        applyFilters()
+    }
+
+    fun applyProfessionFilter(professionOption: String) {
+        activeProfessionFilter = professionOption
+        applyFilters()
+    }
+
+    private fun applyFilters() {
+        if (activeGenderFilter.isEmpty() && activeProfessionFilter.isEmpty()) {
+            if (filterIsActive) {
+                oompaLoompaListLiveData.postValue(oompaLoompaList)
+            }
+            filterIsActive = false
+        } else {
+            oompaLoompaFilteredList.clear()
+            oompaLoompaFilteredList.addAll(
+                oompaLoompaList.filter { oompaLoompa ->
+                    var genderMatch = true
+                    var professionMatch = true
+                    activeGenderFilter.takeIf { it.isNotEmpty() }?.let { filter ->
+                        oompaLoompa.gender?.let { gndr ->
+                            genderMatch = gndr == filter
+                        }
+                    }
+
+                    activeProfessionFilter.takeIf { it.isNotEmpty() }?.let { filter ->
+                        oompaLoompa.profession?.let { prof ->
+                            professionMatch = prof == filter
+                        }
+                    }
+
+                    genderMatch && professionMatch
+                }
+            )
+
+            filterIsActive = true
+            if (oompaLoompaFilteredList.size < THRESHOLD) {
+                getNewOompaLoompasPage()
+            }
+            oompaLoompaListLiveData.postValue(oompaLoompaFilteredList)
+        }
     }
 }
