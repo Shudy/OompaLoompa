25b61d73e772f4a36fae537710821c7731c197fb
Elias Ortiz <elias.ortizreyes@gmail.com>
2021-05-12 12:45:15 +0200

    added splash and icon

--- app/src/main/java/com/eliasortiz/oompaloomparrhh/ui/main/MainActivity.kt
+++ app/src/main/java/com/eliasortiz/oompaloomparrhh/ui/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.eliasortiz.oompaloomparrhh.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.eliasortiz.oompaloomparrhh.R
 import com.eliasortiz.oompaloomparrhh.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_OompaLoompaRRHH)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
